26c115e3624fa7bb4ce7c9c4b379a408ff22a2e9
John <John>
2016-10-06 14:38:21 -0700

    woops xD

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,7 +2,8 @@ hometown <- "Seattle, Washington"
 my.name <- "John Soter"
 my.height <- "6ft"
 puppies <- 2
-puppy <- 100
-total.cost <- 200
-too.expensive <- FALSE
+puppy.cost <- 100
+total.cost <- puppies * puppy.cost
+too.expensive <- total.cost > 1000
 max.puppies <- 10
+
